ca624b0da84f639dd2e3c8a66acd3467c08339ad
Paulo Santos | Casa <phs.developer@gmail.com>
2021-08-15 14:18:09 -0300

    03. Testando funcionalidades de leilão - Aula 03: Obtendo elementos.

--- Selenium.Tests/Alura.LeilaoOnline.Selenium/Testes/AoEfetuarRegistro.cs
+++ Selenium.Tests/Alura.LeilaoOnline.Selenium/Testes/AoEfetuarRegistro.cs
@@ -7,6 +7,7 @@ using Xunit;
 
 namespace Alura.LeilaoOnline.Selenium.Testes
 {
+    [Collection("Chrome Driver")]
     public class AoEfetuarRegistro
     {
         private IWebDriver driver;
@@ -19,12 +20,35 @@ namespace Alura.LeilaoOnline.Selenium.Testes
         [Fact]
         public void Teste()
         {
-            //Arrange - Dado chorme aberto, pagina incial do sistam de leilões
-            //Dados de registro validos ingormados
+            /*************************************************************************
+             * Arrange - Dado chorme aberto, pagina incial do sistam de leilões      *
+             * Dados de registro validos ingormados                                  *
+            **************************************************************************/
             driver.Navigate().GoToUrl("http://localhost:5000");
+
+
+            IWebElement inputNome = driver.FindElement(By.Id("Nome"));
+            IWebElement inputEmail = driver.FindElement(By.Id("Email"));
+            IWebElement inputSenha = driver.FindElement(By.Id("Password"));
+            IWebElement inputConfirmaSenha = driver.FindElement(By.Id("ConfirmPassword"));
+            IWebElement BotaoRegistro = driver.FindElement(By.Id("btnRegistro"));
+
+            inputNome.SendKeys("Paulo Santos");
+            System.Threading.Thread.Sleep(300);
+            inputEmail.SendKeys("paulo.santos@teste.com.br");
+            System.Threading.Thread.Sleep(300);
+            inputSenha.SendKeys("123");
+            System.Threading.Thread.Sleep(300);
+            inputConfirmaSenha.SendKeys("123");
+            System.Threading.Thread.Sleep(300);
+
+
             //act - efetuo o registro
 
+            BotaoRegistro.Click();
+
             //assert - devo ser direcionado para uma pagina de agradecimento
+            Assert.Contains("Obrigado", driver.PageSource);
         }
     }
 }
